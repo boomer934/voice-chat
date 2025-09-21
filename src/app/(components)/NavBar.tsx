c66442d58baa7a6e4682e632855d3044fcf9b3dc
boomer934 <lupomarino09@gmail.com>
2025-09-21 19:40:40 +0200

    fix per telefono

--- src/app/(components)/NavBar.tsx
+++ src/app/(components)/NavBar.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import Image from 'next/image'
 export default function NavBar() {
   return (
-    <div className='w-full h-16 shadow-md text-white flex items-center justify-between px-4'>
+    <div className='w-full h-16 shadow-md text-white flex items-center justify-between px-4 pt-[env(safe-area-inset-top)]'>
         <Image src={"/chatbot.avif"} alt='logo' width={60} height={60} className='rounded-full'/>
         <h1 className='text-2xl font-bold text-black'>Bibo Virtual Assistant</h1>
     </div>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html 
+    lang="en">
+      <head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, viewport-fit=cover"
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col sm:items-center `}
       >
